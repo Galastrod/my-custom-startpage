357398527d34b326e114a82708ba71202fb40c5c
Galastrod <albertmagnus829@gmail.com>
2026-08-21 15:38:22 +0300

    Анимация колонок

---
+++ conf.js
@@ -0,0 +1,75 @@
+const data = [
+  {
+    name: "Anime",
+    content: [
+      {
+        name: "Amedia",
+        href: "https://amd.online/"
+      },
+      {
+        name: "Dreamerscast",
+        href: "https://dreamerscast.com/"
+      },
+      {
+        name: "Jut Su",
+        href: "https://jut.su/"
+      }
+    ]
+  },
+{
+  name: "Net tools",
+  content: [
+    {
+      name: "GitHub",
+      href: "https://github.com"
+    },
+    {
+      name: "GitLab",
+      href: "https://gitlab.com"
+    },
+    {
+      name: "Time web",
+      href: "https://timeweb.cloud/my/projects/1257287"
+    }
+  ]
+},
+{
+  name: "Apps",
+  content: [
+    {
+      name: "ChatGPT",
+      href: "https://chat.openai.com"
+    },
+    {
+      name: "Youtube",
+      href: "https://www.youtube.com/"
+    }
+  ]
+},
+{
+  name: "Social",
+  content: [
+    {
+      name: "Pinterest",
+      href: "https://www.pinterest.com/"
+    },
+    {
+      name: "Vk",
+      href: "https://vk.com/feed"
+    },
+  ]
+},
+{
+  name: "Local tools",
+  content: [
+    {
+      name: "TP-link",
+      href: "http://192.168.0.1/"
+    },
+    {
+      name: "Led tools",
+      href: "https://localhost:11987/#/"
+    }
+  ]
+}
+];
